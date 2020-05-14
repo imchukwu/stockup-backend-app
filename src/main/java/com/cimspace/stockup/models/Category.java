94f01eb5b57391549e10c76a437be396ddaa94d7
Ike Chukwu <imchukwu@gmail.com>
2020-05-14 22:42:43 +0100

    created category model. Closes #5

--- src/main/java/com/cimspace/stockup/models/Category.java
+++ src/main/java/com/cimspace/stockup/models/Category.java
@@ -1,4 +1,24 @@
 package com.cimspace.stockup.models;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = {"products"})
+@Entity
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products;
+
 }
